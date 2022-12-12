5a86a218eb67e2ef17e9ed9261ded2d9017371ca
HarshaSalih <harshasalih2000@gmail.com>
2022-12-13 00:20:41 +0530

    commits

---
+++ src/main/java/com/example/Nestdigitalnewapp_backend/dao/LeaveDao.java
@@ -0,0 +1,24 @@
+package com.example.Nestdigitalnewapp_backend.dao;
+
+import com.example.Nestdigitalnewapp_backend.model.Leave;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Map;
+
+
+public interface LeaveDao extends CrudRepository<Leave,Integer> {
+
+    @Modifying
+    @Query(value = "UPDATE `leaves` SET `status`=:status WHERE `id`=:id",nativeQuery = true)
+    void updateById(@Param("id")Integer id, @Param("status")Integer status);
+
+    @Query(value = "SELECT l.`id`, l.`apply_date`, l.`emp_id`, l.`from_date`, l.`remarks`, l.`status`, l.`to_date`,e.`name`,e.`designation` `type` FROM `leaves` l JOIN employee e ON l.`emp_id`=e.`id` WHERE `status`=0",nativeQuery = true)
+    List<Map<String,String>> viewAllLeaveBy();
+
+    @Query(value = "SELECT l.`id`, l.`apply_date`, l.`emp_id`, l.`from_date`, l.`remarks`, l.`status`, l.`to_date`,l.`type`,e.`name`,e.`designation` `type` FROM `leaves` l JOIN employee e ON l.`emp_id`=e.`id` WHERE l.emp_id=:emp_id",nativeQuery = true)
+    List<Map<String,String>> viewLeaveById(@Param("emp_id")Integer emp_id);
+}
---
+++ src/main/java/com/example/Nestdigitalnewapp_backend/model/Leave.java
@@ -0,0 +1,99 @@
+package com.example.Nestdigitalnewapp_backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "leaves")
+
+public class Leave {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private int empId;
+    private String type;
+    private String remarks;
+    private String fromDate;
+    private String toDate;
+    private String applyDate;
+    private int status;
+
+    public Leave() {
+    }
+
+    public Leave(int id, int empId, String type, String remarks, String fromDate, String toDate, String applyDate, int status) {
+        this.id = id;
+        this.empId = empId;
+        this.type = type;
+        this.remarks = remarks;
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.applyDate = applyDate;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(String fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(String toDate) {
+        this.toDate = toDate;
+    }
+
+    public String getApplyDate() {
+        return applyDate;
+    }
+
+    public void setApplyDate(String applyDate) {
+        this.applyDate = applyDate;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
